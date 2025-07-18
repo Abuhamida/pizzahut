2b009b6a8c98ac38d508c86029e490695d5d8e09
Abuhamida <mr6303302@gmail.com>
2025-07-19 00:05:16 +0300

    update coming soon page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
-import Link from 'next/link'
- 
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen p-4 text-center '>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 py-28 text-center ">
+      <h2 className="font-fugaze text-4xl font-bold">Coming Soon</h2>
+      <Link
+        href="/"
+        className="text-white bg-[#ee3a430] text-xl font-nunito font-bold rounded-2xl cursor-pointer px-4 py-2 mt-2"
+      >
+        Go back to Home
+      </Link>
     </div>
-  )
+  );
 }
