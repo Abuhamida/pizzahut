f1c87745816613ff4811f616773bcdbbf208390d
Abuhamida <mr6303302@gmail.com>
2025-07-19 00:07:21 +0300

    update coming soon page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h2 className="font-fugaze text-4xl font-bold">Coming Soon</h2>
       <Link
         href="/"
-        className="text-white bg-[#ee3a430] text-xl font-nunito font-bold rounded-2xl cursor-pointer px-4 py-2 mt-2"
+        className="text-white bg-[#ee3a43] text-xl font-nunito font-bold rounded-2xl cursor-pointer px-4 py-2 mt-2"
       >
         Go back to Home
       </Link>
